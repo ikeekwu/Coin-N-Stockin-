934d5f302ac53739527417ae45cac558f2ad6a40
Kendall Arceo <arceokendall@gmail.com>
2020-05-25 11:15:49 -0500

    Playing with components and navbars.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,32 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { Fragment } from "react";
+import Header from './Components/Layouts/Header';
+import Footer from './Components/Layouts/Footer';
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
-}
 
+function App (){
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
+
+  return (
+  
+  <ThemeProvider theme = {darkTheme}>
+
+    <Fragment>
+      <Header/>
+
+      <Footer/>
+    </Fragment>
+  </ThemeProvider>
+    
+  
+  )
+  
+}
 
 export default App;
---
+++ client/src/Components/Layouts/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default props =>
+    <div>
+        <h1>Footer</h1>
+    </div>
