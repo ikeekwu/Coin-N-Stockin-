7de1b20acabccbad499be527f606755b6e0d17a1
Kendall Arceo <arceokendall@gmail.com>
2020-05-30 22:35:08 -0500

    changed file names to index to prevent failure in future deployment for Heroku.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Footer from './Components/Layouts/Footer';
-import SignIn from './Components/SignInPage/SignIn';
-import SignUp from './Components/SignUpPage/SignUp';
+import Footer from './Components/Footer/index';
+import SignIn from './Components/SignInPage/index';
+import SignUp from './Components/SignUpPage/index';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import "./App.css";
