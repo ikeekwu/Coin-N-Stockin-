381b04dd2f862502c9c862b04a0707ef9201e00c
Ike Ekwueme <cryptoekwu@gmail.com>
2020-05-30 23:04:36 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,12 +16,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes)
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/User", {useNewUrlParser: true});
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
