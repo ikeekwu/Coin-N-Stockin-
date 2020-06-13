d48f68fdb5d4b6a97efb6cec61b4aa51a5027871
Kendall Arceo <arceokendall@gmail.com>
2020-06-13 01:38:07 -0500

    Revert "Going to try to merge into master"

--- StocksN/client/src/index.js
+++
@@ -1,9 +0,0 @@
-// React
-import React from "react";
-import ReactDOM from "react-dom";
-import "./utils/CSS/App.css";
-import App from "./App";
-import registerServiceWorker from "./utils/JS/registerServiceWorker";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
