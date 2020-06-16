a581ee9c57c6472e701decdb368a96055b3f9e7c
Jordon <Jordonrunge20@gmail.com>
2020-06-16 18:02:16 -0500

    Connteced DB to Website... I believe

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,10 @@ import axios from "axios";
 // const CMC_PRO_API_KEY = "CMC_PRO_API_KEY=6461f779-9d5a-42a7-b7b1-2676a7c800a4";
 
 export default {
-  // Gets all books
+  // Gets alls
+  userSignIn: function () {
+    return axios.get("/api/account/");
+  },
   getAllStocks: function() {
     return axios.get("/api/stocks/");
   },
